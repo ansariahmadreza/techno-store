3048b31bb8f06dde5c9b81d250ca5a245ef71bc6
Ahmadreza Ansari <ahmadreza.an.dev@gmail.com>
2026-04-25 20:17:06 +0330

    change

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,6 @@
-// import type {Next} from "next";
-
-// const nextConfig = {
-//   async rewrites() {
-//     return [
-//       {
-//         source: '/api/:path*',
-//         destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
-//       },
-//     ];
-//   },
-// };
-
-// module.exports = nextConfig;
+const nextConfig = {
+  output: 'export',   
+  images: {
+    unoptimized: true,
+  },
+}
